54f6c5650ffb8a38d4a573871bd281af0f3b8dfd
Darko Jovevski <platzmanfan9@gmail.com>
2020-02-17 21:56:47 -0800

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,11 @@ var image;
 var loss;
 var increment = [1,2,3,4,5,6,7,8,9,10,11,12];
 var targetNumber;
+var empty;
+var array;
 var options
 var counterTotal;
+// we create functions to get elements, set the number and give random numbers!!
 var getElements = function(){
     randomNum = $("#randomNumber");
     win =$("#wins");
@@ -26,75 +29,89 @@ var setElement = function(){
  
 var randomNumber = function(){
     options = increment[Math.floor(Math.random() * increment.length)];
-   return options;
+    return options;
    
 }
+var randomNumber
 var randomDisplay = function(min, max){
     
     targetNumber = Math.floor(Math.random() * (max - min) + min);
     return targetNumber;
-    
+
 }
 var reset = function(){
     counter = 0;
-    $("#counter").text(counter)
+    $("#counter").text(counter);
+        //working till here
+
+       $(".crystal-image").attr("data-image", options);
+       
     
 }
+// we pulling the pictures and creating them via jquery
+
 var createImg = function(){
     
     var crystal = $("<img>");
     var crystal1 =$("<img>");
     var crystal2 =$("<img>");
     var crystal3 =$("<img>");
-    crystal.addClass("crystal-image");
-    crystal1.addClass("crystal-image");
-    crystal2.addClass("crystal-image");
-    crystal3.addClass("crystal-image");
-    crystal.attr("src", "assets/images/crystal-1.png");
-    crystal1.attr("src", "assets/images/crystal-2.png");
-    crystal2.attr("src", "assets/images/crystal-3.png");
-    crystal3.attr("src", "assets/images/crystal-4.png");
-    crystal1.attr("data-image", options);
-    if(!randomNumber === crystal) {$(".crystals").append(crystal); }
-    crystal.attr("data-image" , randomNumber(1,13));
-    if(!randomNumber === crystal1) {$(".crystals").append(crystal1); }
+             crystal.addClass("crystal-image");
+             crystal1.addClass("crystal-image1");
+             crystal2.addClass("crystal-image2");
+             crystal3.addClass("crystal-image3");
+                  crystal.attr("src", "assets/images/crystal-1.png");
+                  crystal1.attr("src", "assets/images/crystal-2.png");
+                  crystal2.attr("src", "assets/images/crystal-3.png");
+                  crystal3.attr("src", "assets/images/crystal-4.png");
+    
+    
+    crystal.attr("data-image", randomNumber(1,13));
+    
     crystal1.attr("data-image" , randomNumber(1,13));
-    if(!randomNumber === crystal2){$(".crystals").append(crystal2); }
+    
     crystal2.attr("data-image" , randomNumber(1,13));
-    if(!randomNumber === crystal3) {$(".crystals").append(crystal3); }
+    
     crystal3.attr("data-image" , randomNumber(1,13));
     $(".crystals").append(crystal);
     $(".crystals").append(crystal1);
     $(".crystals").append(crystal2);
     $(".crystals").append(crystal3);
 }
+// when windows load what to show
 $(document).ready(function(){
-    setElement();
-   createImg();
+          setElement();
+          createImg();
    
 });
-
-// console.log(randomDisplay(19,120))
-$(document).on("click", ".crystal-image" , function(){
+var rNumber = function(){
+   
+    $(".crystal-image").attr("data-image", randomNumber(1,13));
+    $(".crystal-image1").attr("data-image", randomNumber(1,13));
+    $(".crystal-image2").attr("data-image", randomNumber(1,13));
+    $(".crystal-image3").attr("data-image", randomNumber(1,13));
+}
+//// the event click with the if statements
+$(document).on("click touchend" , ".crystal-image , .crystal-image1, .crystal-image2, .crystal-image3",  function(){
     var value = ($(this).attr("data-image"));
-    value = parseInt(value);
-    counter += value;
-    $("#counter").text(counter);
+         value = parseInt(value);
+         counter += value;
+         $("#counter").text(counter);
     
     if(counter === targetNumber){
         wins++;
         $("#wins").text(wins);
         setElement();
         reset();
+        rNumber();
     }
     else if (counter > targetNumber){
         losses++;
         $("#losses").text(losses);
         setElement();
         reset();
+        rNumber();
+       
     }
-
-
-
 });
 
